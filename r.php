8c6d5174e8bf859cde88ae094874a0acc445c480
Amir <124592091+amirmmafakheri@users.noreply.github.com>
2023-08-22 16:49:56 +0330

    Update r.php

--- r.php
+++ r.php
@@ -1,16 +1,7 @@
 <?php
-if(isset($_GET['url']) && !empty($_GET['url'])){
-    $url = $_GET['url'];
-    
-    // Validate the URL (optional)
-    if(filter_var($url, FILTER_VALIDATE_URL)){
-        // Redirect the user to the specified URL
-        header("Location: $url");
-        exit();
-    } else {
-        echo "Invalid URL. Please enter a valid URL.";
-    }
-} else {
-    echo "URL parameter is missing. Please enter a URL.";
+if($_GET["url"]) {
+    $target = $_GET["url"];
+    header("Location: $target");
+}else {
+    echo "No url";
 }
-?>
